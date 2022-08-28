2657d2b68653b67cd0081e170f907ef34d1191c9
Ketan3102 <112306805+Ketan3102@users.noreply.github.com>
2022-08-28 12:48:24 +0530

    Add files via upload

---
+++ 01dictionary.py
@@ -0,0 +1,28 @@
+a = {
+    "Hello" : "Greeings", #here, Hello is the Key and Greetings is the value.. just like dictionary
+    "Hello" : "Swagat", #can be overwrited
+    "Ketan" : "Word",
+    False: [1,2,3],
+    "d": {
+        "e":"Hero" #we added another dict as well
+    }
+}
+# print(a["d"]["e"]) 
+# a["d"]={
+#     "f":"Go fast"
+# }
+# a['Hello']="Swagat" #can be mutated
+# b={
+#         "Sad": "Dukhi"   
+# }
+# a.update(b)  ##updates the dict
+# print(a)
+# print(a["Hello"]) #to display value of key
+# print(a[False])
+# print(a["d"]["f"]) # to print that value of dict from another dict
+# print(a.keys()) #prints the keys of dict
+# print(a.values()) #prints the values of dist
+# print(a.items()) #prints the items(keys,values) of dist
+
+# print(type(a.keys()))
+# print(list(a.keys())) #makes it list
